dfd98dd91d8840e7f37dfb6aed0e87f898137405
T-Böhse <tobias.boyke+bitlc@gmail.com>
2025-12-03 21:32:33 +0100

    feat: Add Mermaid diagram support, implement a light/dark/system theme switcher, and update core styling.

--- js/site.js
+++ js/site.js
@@ -1,9 +1,11 @@
+import mermaid from 'mermaid';
+
 var isTouch = window.DocumentTouch && document instanceof DocumentTouch;
 
 function scrollHeader() {
     // Has scrolled class on header
     var zvalue = $(document).scrollTop();
-    if ( zvalue > 75 )
+    if (zvalue > 75)
         $("#header").addClass("scrolled");
     else
         $("#header").removeClass("scrolled");
@@ -13,13 +15,22 @@ function parallaxBackground() {
     $('.parallax').css('background-positionY', ($(window).scrollTop() * 0.3) + 'px');
 }
 
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
+
+    // Initialize Mermaid
+    mermaid.initialize({
+        startOnLoad: true,
+        theme: 'base',
+        themeVariables: {
+            fontFamily: 'inherit'
+        }
+    });
 
     scrollHeader();
 
     // Scroll Events
-    if (!isTouch){
-        $(document).scroll(function() {
+    if (!isTouch) {
+        $(document).scroll(function () {
             scrollHeader();
             parallaxBackground();
         });
@@ -27,13 +38,13 @@ jQuery(document).ready(function($){
 
     // Touch scroll
     $(document).on({
-        'touchmove': function(e) {
+        'touchmove': function (e) {
             scrollHeader(); // Replace this with your code.
         }
     });
 
     //Smooth scroll to start
-    $('#to-start').click(function(){
+    $('#to-start').click(function () {
         var start_y = $('#start').position().top;
         var header_offset = 45;
         window.scroll({ top: start_y - header_offset, left: 0, behavior: 'smooth' });
@@ -41,7 +52,7 @@ jQuery(document).ready(function($){
     });
 
     //Smooth scroll to top
-    $('#to-top').click(function(){
+    $('#to-top').click(function () {
         window.scroll({ top: 0, left: 0, behavior: 'smooth' });
         return false;
     });
@@ -54,7 +65,7 @@ jQuery(document).ready(function($){
     });
 
     // Tree Menu
-    $(".tree").treemenu({delay:300});
+    $(".tree").treemenu({ delay: 300 });
 
     // Theme Switcher
     const themeRadios = document.querySelectorAll('input[name="theme"]');
@@ -76,7 +87,7 @@ jQuery(document).ready(function($){
                 html.classList.remove('dark');
             }
         }
-        
+
         // Update radio buttons
         themeRadios.forEach(radio => {
             if (radio.value === theme) {
